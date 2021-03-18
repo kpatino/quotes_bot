c68eb23504d435b31dbd8f137c19723747788a97
kpatino <44142894+kpatino@users.noreply.github.com>
2021-03-17 19:07:07 -0500

    update db functions for new db structure

--- jamal_bot_core.py
+++ jamal_bot_core.py
@@ -18,17 +18,22 @@ handler.setFormatter(logging.Formatter(
     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+# admin role ID must be an integer
+ADMIN_ROLE_ID = 123456
+OWNER_ID = 123456
+
 
 def get_prefix(client, message):  # Function creates prefixes
     # sets the prefixes, future regex here?
-    prefixes = ['jamal ', 'Jamal ', 'JAMAL ']
+    #prefixes = ['jamal ', 'Jamal ', 'JAMAL ']
+    prefixes = ['lamar ']
     return commands.when_mentioned_or(*prefixes)(client, message)
     # in summary allow users to @mention the bot and use three variations of "jamal "
 
 
 # requires jamal with a space in order to register
 jamal_bot = commands.Bot(command_prefix=get_prefix,
-                         case_insensitive=True, owner_id="DISCORD_ID")
+                         case_insensitive=True, owner_id=OWNER_ID)
 jamal_bot.remove_command('help')  # remove the default help command
 
 # jamal_bot commands
@@ -36,8 +41,10 @@ jamal_bot.remove_command('help')  # remove the default help command
 
 @jamal_bot.event  # jamal connection to discord api
 async def on_ready():
-    print(print(
-        f'\n\nLogged in as: {jamal_bot.user.name} - {jamal_bot.user.id}\nVersion: {discord.__version__}\n'))
+    print(
+        f'\nLogged in as: {jamal_bot.user.name} - {jamal_bot.user.id}')
+    print(
+        f'Version: {discord.__version__}')
     activity = discord.Game(name='Warframe')
     await jamal_bot.change_presence(status=discord.Status.online, activity=activity)
     print(f'Done')  # Printing done let's pterodactyl know that it's ready
@@ -53,14 +60,14 @@ async def on_command_error(ctx, error):
 @jamal_bot.command()  # jamal lookup
 @commands.guild_only()  # ignore in DMs
 async def lookup(ctx):
-    await ctx.send(f'{list_tables()}')
+    await ctx.send(f'{get_names()}')
 
 
 @jamal_bot.command()  # jamal access {name}
 @commands.guild_only()  # ignore in DMs
 async def access(ctx, name):
     name = name.lower()  # set name to lowercase just in case
-    if check_tables(name) == True:  # check if the name is in the database
+    if check_name(name) == True:  # check if the name is in the database
         await ctx.send(f'{get_quote(name)}')
     else:
         await ctx.send(f'"{name}" is not in the database')
@@ -69,10 +76,11 @@ async def access(ctx, name):
 @jamal_bot.command()  # jamal add {name} "{quote}"
 @commands.guild_only()  # ignore in DMs
 # must have these roles in order to add quotes
-@commands.has_any_role("ROLE_ID")
+@commands.has_any_role(ADMIN_ROLE_ID)
 async def add(ctx, name, quote: str):
     name = name.lower()  # set name to lowercase just in case
-    if check_tables(name) == False:  # check if name is in the database
+    print (f"{name} and {quote}")
+    if check_name(name) == False:  # check if name is in the database
         await ctx.send(f'"{name}" is not in the database')
     else:
         add_quote(name, quote)  # add_quote function
@@ -83,7 +91,7 @@ async def add(ctx, name, quote: str):
 @jamal_bot.command()  # random quote game
 @commands.guild_only()  # ignore in DMs
 async def quotes(ctx, pass_context=True):
-    name = random_table()
+    name = random_name()
     await ctx.send(f'who said "{get_quote(name)}"')
 
     try:
@@ -91,7 +99,7 @@ async def quotes(ctx, pass_context=True):
         guess = await jamal_bot.wait_for('message', timeout=6.0)
     except asyncio.TimeoutError:
         # if no guess is provided in 5 seconds stop waiting
-        return await ctx.channel.send(f'you\'re dragging it was {name}')
+        return await ctx.channel.send(f'you\'re taking too long, it was {name}')
 
     if (str(guess.content)).lower() == name:  # if guess matches namme
         await ctx.channel.send('you got em')
@@ -105,15 +113,15 @@ async def help(ctx):
     help_embed = discord.Embed(colour=discord.Colour.blurple())
     help_embed.set_author(name='jamal help')
     help_embed.add_field(
-        name='jamal help', value='jamal will display this message for your dumbass to read', inline=False)
+        name='jamal help', value='jamal will display the help message', inline=False)
     help_embed.add_field(
-        name='jamal lookup', value='jamal will display all the names in the database', inline=False)
+        name='jamal lookup', value='jamal will display all available names in the database', inline=False)
     help_embed.add_field(
-        name='jamal quotes', value='jamal will give a quote and you guess who said it, it\'s simple', inline=False)
+        name='jamal quotes', value='jamal will give a random quote and you guess who said it', inline=False)
     help_embed.add_field(name='jamal access <name>',
                          value='jamal will send a random quote from someone in the database', inline=False)
     help_embed.add_field(name='jamal add <name> "<quote>"',
-                         value='jamal will add a quote to the database, but use the fucking double quotes when you\'re adding new ones please', inline=False)
+                         value='jamal will add a quote to the database, but use the double quotes when you\'re adding new ones please', inline=False)
     help_embed.set_footer(
         text='commands are mostly case insensitive, try to always use lowercase just in case')
     await ctx.send(embed=help_embed)  # actually send the embed
@@ -137,22 +145,20 @@ class open_db(object):  # sqlite ctx manager i found on github, seems to work fo
         self.conn.close()
 
 
-def list_tables():
+def get_names():
     with open_db('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
-            """SELECT name FROM sqlite_master WHERE type='table';""")
-        tables = [
+            """SELECT name FROM people;""")
+        names = [
             v[0] for v in cursor.fetchall()
-            if v[0] != "sqlite_sequence"
         ]
-        tables = ', '.join(map(str, tables,))
-        return(tables)
+        names = ', '.join(map(str, names,))
+        return(names)
 
-
-def check_tables(table):  # check if the table exists to prevent a crash
+def check_name(name):  # check if the table exists to prevent a crash
     with open_db('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
-            f"""SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table}';""")
+            f"""SELECT count(name) FROM people WHERE name='{name}';""")
         if cursor.fetchone()[0] == 1:
             return(True)
         else:
@@ -161,24 +167,24 @@ def check_tables(table):  # check if the table exists to prevent a crash
 
 def get_quote(name):  # get quote from database
     with open_db('./jamal_bot_quotes.db') as cursor:
-        cursor.execute(f"""SELECT * FROM {name} ORDER BY RANDOM() LIMIT 1;""")
+        cursor.execute(f"""SELECT quote FROM quotes WHERE name= "{name}" ORDER BY RANDOM() LIMIT 1;""")
         return(cursor.fetchone()[0])
 
 
 def add_quote(name, quote):  # add quote to database
     with open_db('./jamal_bot_quotes.db') as cursor:
-        cursor.execute(f"""INSERT INTO {name} (quotes) VALUES("{quote}");""")
+        cursor.execute(f"""INSERT INTO "main"."quotes" ("name", "quote") VALUES ('{name}', '{quote}');""")
+        print(name + quote)
 
 
-def random_table():  # get a random table
+def random_name():  # get a random table
     with open_db('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
-            """SELECT name FROM sqlite_master WHERE type='table';""")
-        tables = [
+            """SELECT name FROM people;""")
+        names = [
             v[0] for v in cursor.fetchall()
-            if v[0] != "sqlite_sequence"
         ]
-        name = random.choice(tables)
+        name = random.choice(names)
         return(name)
 
 
