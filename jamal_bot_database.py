55cf686821828165961868a50f4c89a33d02c4a6
kpatino <44142894+kpatino@users.noreply.github.com>
2021-09-03 22:03:01 -0500

    add docstrings to database functions

--- jamal_bot_database.py
+++ jamal_bot_database.py
@@ -3,6 +3,12 @@ import random
 
 
 class OpenDatabase(object):
+    """
+    File context manager used for opening and closing database connections
+
+    Args:
+        object (str): filepath of the database file
+    """
     def __init__(self, path):
         self.path = path
 
@@ -17,6 +23,12 @@ class OpenDatabase(object):
 
 
 def create_db(db_name):
+    """
+    Creates a database with tables that the bot can use
+
+    Args:
+        db_name (str): name of the database file
+    """
     create_people_table = """CREATE TABLE IF NOT EXISTS people(
                                 'name' TEXT NOT NULL UNIQUE
                             );"""
@@ -34,6 +46,7 @@ def create_db(db_name):
 
 
 def get_names():
+    """Retrive all names in "people" database."""
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
             "SELECT name FROM people")
@@ -45,17 +58,36 @@ def get_names():
 
 
 def add_name(name):
+    """
+    Adds a name to the bot's database specifically the "people" table.
+
+    Args:
+        name (str): name that should added into the bot's database.
+    """
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute("INSERT INTO people ('name') VALUES (?)", (name,))
 
 
 def remove_name(name):
+    """
+    Remove a name and all their quotes from the bot's database.
+    This action is not reversible.
+
+    Args:
+        name (str): name that should be removed from the database if it exists.
+    """
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute("DELETE FROM quotes WHERE name == (?);", (name,))
         cursor.execute("DELETE FROM people WHERE name == (?);", (name,))
 
 
 def check_name(name):
+    """
+    Checks to see if the name exists in the database table "people".
+
+    Args:
+        name (str): name that should be checked if it exists
+    """
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute("SELECT count(name) FROM people WHERE name=?", (name,))
         if cursor.fetchone()[0] == 1:
@@ -65,6 +97,12 @@ def check_name(name):
 
 
 def get_quote(name):
+    """
+    Retrieves random quote from the database by name in the table "quotes"
+
+    Args:
+        name (str): name used to retrieve a random quote
+    """
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
             "SELECT quote FROM quotes WHERE name=? ORDER BY RANDOM() LIMIT 1", (name,))
@@ -72,12 +110,19 @@ def get_quote(name):
 
 
 def add_quote(name, quote):
+    """
+    Add quote to the bot's database using string variables name and quote
+    Args:
+        name (str): name used for database entry
+        quote (str): quot used for database entry
+    """
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
             "INSERT INTO quotes ('name', 'quote') VALUES (?, ?)", (name, quote,))
 
 
 def random_name():
+    """Retrieve random name from "people" table"""
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute(
             "SELECT name FROM people")
@@ -89,6 +134,12 @@ def random_name():
 
 
 def list_quotes(name):
+    """
+    Retrieve all quotes from "quotes" table by name
+
+    Args:
+        name (str): name used to retrieve all quotes
+    """
     with OpenDatabase('./jamal_bot_quotes.db') as cursor:
         cursor.execute("SELECT * FROM quotes WHERE name == (?);", (name,))
         items = cursor.fetchall()
