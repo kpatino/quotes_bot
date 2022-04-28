a743ba3913014c507465189ef49a31d8595c4c5d
Kevin Patino <44142894+kpatino@users.noreply.github.com>
2022-04-28 16:11:49 -0500

    Unify access command function

--- jamal_bot_core.py
+++ jamal_bot_core.py
@@ -97,25 +97,24 @@ async def slash_list(inter):
     await inter.response.send_message(jamal_bot_database.get_names())
 
 
+def access_command(name: str):
+    name = name.lower()
+    if jamal_bot_database.check_name(name) is True:
+        return(jamal_bot_database.get_quote(name))
+    else:
+        return(f'The name "{name}" is not in the database')
+
+
 @jamal_bot.command(description='Access a random quote by name')
 async def access(ctx, input_name: str):
-    input_name = input_name.lower()
-    if jamal_bot_database.check_name(input_name) is True:
-        await ctx.send(jamal_bot_database.get_quote(input_name))
-    else:
-        await ctx.send(f'The name "{input_name}" is not in the database')
+    await ctx.send(access_command(input_name))
 
 
 @jamal_bot.slash_command(
     name='access',
     description='Access a random quote by name')
 async def slash_access(inter: disnake.CommandInteraction, name: str):
-    name = name.lower()
-    if jamal_bot_database.check_name(name) is True:
-        await inter.response.send_message(jamal_bot_database.get_quote(name))
-    else:
-        await inter.response.send_message(f'The name "{name}" is not in the '
-                                          'database')
+    await inter.response.send_message(access_command(name))
 
 
 @slash_access.autocomplete('name')
