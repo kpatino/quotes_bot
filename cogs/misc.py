dc7d0711316c685f52da7f913dfc8205161651d8
Kevin Patiño <kevin@kevinpatino.com>
2023-05-12 21:31:47 -0500

    MiscFunctions class doesn't need to exist

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,35 +3,32 @@
 
 from datetime import datetime
 
-import asyncio
 import disnake
 import pytz
-import logging
 from disnake.ext import commands
 
 from config import Config
 
 
-class MiscFunctions():
-    async def timezone_embed():
-        """
-        Create an embed with the current time in different timezones and return it.
+async def timezone_embed():
+    """
+    Create an embed with the current time in different timezones and return it.
 
-        Returns
-            embed: Time in different timezones
-        """
+    Returns
+        embed: Time in different timezones
+    """
 
-        embed = disnake.Embed(colour=disnake.Colour.purple())
-        embed.set_author(name='Time')
+    embed = disnake.Embed(colour=disnake.Colour.purple())
+    embed.set_author(name='Time')
 
-        for tz in Config.timezone_list:
-            embed.add_field(
-                name=tz,
-                value=datetime.now(
-                    pytz.timezone(tz)).strftime('%b %d %I:%M %p (%H:%M)'),
-                inline=False)
+    for tz in Config.timezone_list:
+        embed.add_field(
+            name=tz,
+            value=datetime.now(
+                pytz.timezone(tz)).strftime('%b %d %I:%M %p (%H:%M)'),
+            inline=False)
 
-        return embed
+    return embed
 
 
 class MiscCommands(commands.Cog):
@@ -40,13 +37,13 @@ class MiscCommands(commands.Cog):
 
     @commands.command(description='Get the current time in different timezones')
     async def time(self, inter):
-        await self.send(embed=await MiscFunctions.timezone_embed())
+        await self.send(embed=await timezone_embed())
 
     @commands.slash_command(
         name='time',
         description='Get the current time in different timezones')
     async def slash_time(self, inter):
-        await inter.response.send_message(embed=await MiscFunctions.timezone_embed())
+        await inter.response.send_message(embed=await timezone_embed())
 
 
 def setup(bot):
