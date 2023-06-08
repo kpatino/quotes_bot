kpatino/quotes_bot

commit be7543db648a0711c60a374a5deb655546258221

Only create logs folder when bot.py is run

bot.py
@@ -8,8 +8,6 @@ from disnake.ext import commands
 import database
 from config import Config
 
-# Create logs folder if it doesn't exist
-os.makedirs("logs", exist_ok=True)
 
 # Logging configuration
 log_format = '[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s'
@@ -60,6 +58,9 @@ class JamalBot(commands.Bot):
 
 
 if __name__ == '__main__':
+    # Create folders if they doesn't exist
+    os.makedirs("logs", exist_ok=True)
+
     # Only creates the database if it doesn't exist
     database.create_db('quotes.db')
 

commit f61b91f71af7d59e1e5462032dcbf804fe24ddad

Fix missing logs folder Caused by be7543d

bot.py
@@ -9,6 +9,12 @@ import database
 from config import Config
 
 
+try:
+    os.makedirs("logs", exist_ok=True)
+    print("Directory logs created successfully")
+except OSError:
+    print("Directory logs can not be created")
+
 # Logging configuration
 log_format = '[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s'
 date_format = '%Y-%m-%d %H:%M:%S'
@@ -58,9 +64,6 @@ class JamalBot(commands.Bot):
 
 
 if __name__ == '__main__':
-    # Create folders if they doesn't exist
-    os.makedirs("logs", exist_ok=True)
-
     # Only creates the database if it doesn't exist
     database.create_db('quotes.db')
 