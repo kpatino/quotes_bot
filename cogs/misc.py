01288e53c8d435271b596ed0bd44a321bd55aba6
Kevin Patiño <kevin@kevinpatino.com>
2023-06-27 19:32:11 -0500

    Add some logging to misc cog Also check for empty timezone list

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,7 +13,7 @@ from config import Config
 module_logger = logging.getLogger(f'__main__.{__name__}')
 
 
-async def timezone_embed():
+async def timezone_embed() -> disnake.Embed:
     """
     Create an embed with the current time in different timezones and return it.
 
@@ -21,15 +21,24 @@ async def timezone_embed():
         embed: Time in different timezones
     """
 
-    embed = disnake.Embed(colour=disnake.Colour.purple())
-    embed.set_author(name='Time')
+    embed = disnake.Embed(title='Time')
 
-    for tz in Config.timezone_list:
+    if Config.timezone_list:
+        module_logger.debug(f'Using the following timezones: {Config.timezone_list}')
+        embed.set_default_colour(disnake.Colour.purple())
+        for tz in Config.timezone_list:
+            embed.add_field(
+                name=tz,
+                value=datetime.now(
+                    pytz.timezone(tz)).strftime('%b %d %I:%M %p (%H:%M)'),
+                inline=False)
+    else:
+        module_logger.warn('No timezones listed in .env or in an environment variable')
+        embed.set_default_colour(disnake.Colour.red())
         embed.add_field(
-            name=tz,
-            value=datetime.now(
-                pytz.timezone(tz)).strftime('%b %d %I:%M %p (%H:%M)'),
-            inline=False)
+                name='N/A',
+                value='Timezones were not provided in the config',
+                inline=False)
 
     return embed
 
@@ -40,10 +49,12 @@ class MiscCommands(commands.Cog):
 
     @commands.command(name='time', description='Get the current time in different timezones')
     async def time(self, ctx) -> None:
+        module_logger.info(f'Message command "time" executed by {ctx.author.id}')
         await ctx.reply(embed=await timezone_embed(), mention_author=False)
 
     @commands.slash_command(name='time', description='Get the current time in different timezones')
     async def slash_time(self, inter: disnake.CommandInteraction) -> None:
+        module_logger.info(f'Slash command "time" executed by {inter.author.id}')
         await inter.response.send_message(embed=await timezone_embed())
 
 
