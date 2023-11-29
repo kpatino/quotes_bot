9aa7ddf8887f4d512afd334d6af5c332de7a43c3
Kevin Patiño <kevin@kevinpatino.com>
2023-11-28 21:35:18 -0600

    Added some more logging for quotes commands Will need more debug logging for the cmd functions and for autocomplete

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -103,15 +103,18 @@ class QuotesCommands(commands.Cog):
 
     @commands.command(name='list', description='List available names from the database')
     async def list_names(self, ctx) -> None:
+        module_logger.info(f'Message command "list" executed by {ctx.author.id}')
         await ctx.reply(database.get_names(), mention_author=False)
 
     @commands.slash_command(name='list',
                             description='List available names from the database')
     async def slash_list_names(self, inter: disnake.CommandInteraction) -> None:
+        module_logger.info(f'Slash command "list" executed by {inter.author.id}')
         await inter.response.send_message(database.get_names())
 
     @commands.command(description='Access a random quote by name')
     async def access(self, ctx, input_name: str) -> None:
+        module_logger.info(f'Message command "access" executed by {ctx.author.id}')
         await ctx.reply(access_command(input_name), mention_author=False)
 
     @commands.slash_command(name='access', description='Access a random quote by name',
@@ -120,6 +123,7 @@ class QuotesCommands(commands.Cog):
                                         description='Get a random quote attributed to this name',
                                         required=True)])
     async def slash_access(self, inter: disnake.CommandInteraction, name: str) -> None:
+        module_logger.info(f'Slash command "access" executed by {inter.author.id}')
         await inter.response.send_message(access_command(name))
 
     @slash_access.autocomplete('name')
@@ -135,10 +139,12 @@ class QuotesCommands(commands.Cog):
     @add.command(name='name', description='Add a "name" to the database')
     @commands.has_any_role(Config.discord_admin_role_id, Config.discord_mod_role_id)
     async def add_name(self, ctx, input_name: str) -> None:
+        module_logger.info(f'Message command "add name" with input: [{input_name}] executed by {ctx.author.id}')
         await ctx.reply(add_name_command(ctx.message.author.mention, input_name), mention_author=False)
 
     @add.command(name='quote', description='Add a quote to the database.')
     async def add_quote(self, ctx, input_name: str, *, arg):
+        module_logger.info(f'Message command "add quote" with inputs: [{input_name}] [{arg}] executed by {ctx.author.id}')
         await ctx.reply(add_quote_command(input_name, arg), mention_author=False)
 
     @commands.slash_command(name='add', description='Add a name or quote to the database',
@@ -152,6 +158,7 @@ class QuotesCommands(commands.Cog):
                                         description='Name to add to the database',
                                         required=True)])
     async def slash_add_name(self, inter: disnake.CommandInteraction, name: str) -> None:
+        module_logger.info(f'Message command "add name" with input: [{name}] executed by {inter.author.id}')
         await inter.response.send_message(
             add_name_command(inter.author.mention, name))
 
@@ -165,6 +172,7 @@ class QuotesCommands(commands.Cog):
                                         description='The quote to record to the database',
                                         required=True)])
     async def slash_add_quote(self, inter: disnake.CommandInteraction, name: str, quote: str) -> None:
+        module_logger.info(f'Slash command "add quote" with inputs: [{name}] [{quote}] executed by {inter.author.id}')
         await inter.response.send_message(add_quote_command(name, quote))
 
     @slash_add_quote.autocomplete('name')
@@ -180,6 +188,7 @@ class QuotesCommands(commands.Cog):
     @remove.command(name='name', description='Remove a name and their quotes from the database')
     @commands.has_any_role(Config.discord_admin_role_id, Config.discord_mod_role_id)
     async def rm_name(self, ctx, input_name: str) -> None:
+        module_logger.info(f'Message command "remove name" with inputs: [{input_name}] executed by {ctx.author.id}')
         await ctx.reply(remove_name_command(ctx.message.author.mention, input_name), mention_author=False)
 
     @commands.slash_command(name='remove', description='Remove a name or quote to the database',
@@ -190,6 +199,7 @@ class QuotesCommands(commands.Cog):
     @slash_remove.sub_command(name='name',
                               description='Remove a name and their quotes from the database')
     async def slash_remove_name(self, inter, name: str) -> None:
+        module_logger.info(f'Slash command "remove name" with inputs: [{name}] executed by {inter.author.id}')
         await inter.response.send_message(
             remove_name_command(inter.author.mention, name))
 
@@ -200,6 +210,7 @@ class QuotesCommands(commands.Cog):
 
     @commands.command(description='Get a random quote and guess who said it')
     async def quotes(self, ctx) -> None:
+        module_logger.info(f'Message command "quotes" executed by {ctx.author.id}')
         name = database.get_random_name()
         await ctx.reply(f'Who said “{database.get_random_quote(name)}”', mention_author=False)
 
@@ -216,6 +227,7 @@ class QuotesCommands(commands.Cog):
 
     @commands.slash_command(name='quotes', description='Get a random quote and guess who said it')
     async def slash_quotes(self, inter: disnake.CommandInteraction) -> None:
+        module_logger.info(f'Slash command "quotes" executed by {inter.author.id}')
         name = database.get_random_name()
         await inter.response.send_message(
             f'Who said “{database.get_random_quote(name)}”')
